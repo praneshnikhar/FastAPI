1a51500c9a6a95b9335a8a7d4f18ed726adf08e8
praneshnikhar <praneshnikhar@gmail.com>
2025-08-19 11:57:24 +0530

    query struct

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name' : 'pranesh'}} 
+# @app.get('/blog?limit=10&published= true')
+# def index():
+#     return {'data':{'name' : 'pranesh'}} 
+
+@app.get('/blog')
+def index(limit = 10, published:bool = True, sort: Optional[str]= None):
+    #if u put 10 in url. , then only get 10 published blogs
+    # return published
+    
+    if published: 
+        return {'data':f'{limit} published blogs from the db'} 
+    else:
+        return {'data':f'{limit} blogs from the db'} 
 
 @app.get('/about')
 def about():
@@ -23,6 +33,11 @@ def unpublished():
 ###########################################
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     #fetch comments of blog with id = id
+    return limit
     return {'data':{'1', '2'}}
+
+#use- python -m uvicorn main:app --reload
+#localhost:8000/docs
+#localhost:8000/redoc
