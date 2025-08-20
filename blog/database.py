1ab10e0b4e64109bc70f0cba9a3838ba7693a99f
praneshnikhar <praneshnikhar@gmail.com>
2025-08-20 19:56:06 +0530

    hmm

---
+++ blog/database.py
@@ -0,0 +1,13 @@
+# creating a database 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHAMY_DATABASE_URL = 'sqlite:///./blog.db'
+
+engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread":False})
+
+SessionLocal = sessionmaker(bind= engine, autocommit= False, autoFlush = False)
+
+
+Base = declarative_base()
