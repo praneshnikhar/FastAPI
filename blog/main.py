9db4c34f0a8c6b90b29e7f0c7caba3a8064e461c
praneshnikhar <praneshnikhar@gmail.com>
2025-08-22 15:45:08 +0530

    3:45 not found

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-models.Base.metadata.create_all(engine)
+models.Base.metadata.create_all(bind=engine)   
 def get_db():
     db= SessionLocal()
     try:
@@ -13,20 +13,50 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog', status_code= 201)
+@app.post('/blog', status_code= status.HTTP_201_CREATED)
 def create(request:schemas.Blog,db:Session= Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+# @app.delete('/blog/{id}', status_code = status.HTTP_204_NO_CONTENT)
+# def destroy(id:int, db:Session= Depends(get_db)):
+#     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session= False)
+#     db.commit()
+#     return 'done'    #'Response(status_code=status.HTTP_204_NO_CONTENT)  ' 
+
+@app.put('/blog/{id}', status_code= status.HTTP_202_ACCEPTED)
+def update(id:int, request:schemas.Blog, db:Session= Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).update(request)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not found")
+        
+    blog.delete(synchronize_session= False)
+    db.commit()  
+    return 'updated'
+
     
-@app.get('/blog')
+    
+
+    
+@app.get('/blog', response_model=schemas.ShowBlog)
 def all(db:Session= Depends(get_db)):
-    blogs = db.query(models.Blog).filter(models.Blog.id == id).first()
+    blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def show(id, db:Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog)
+def show(id:int, response : Response , db:Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return{'detail': "Blog with the id {id} is not available"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")   # <-- fixed f-string spacing
     return blog
+
+
+
+@app.post('/user')
+def create_user(request:schemas.User):
+    return request
--- main.py
+++ main.py
@@ -29,16 +29,15 @@ def show(id:str):
                                     #check routing here , in earlier version ,it would go line by lune
                                     # and the add 'unpublished' word in id , in show method , in this version of 
                                     #fast api it would do it perfectly if it matches wit the word unpublished
-@app.get('blog/unpublished')
+@app.get('/blog/unpublished')   # 
 def unpublished():
     return {'data': 'all unpublished blogs'}
 ###########################################
 
 @app.get('/blog/{id}/comments')
-def comments(id, limit=10):
+def comments(id:int, limit:int=10):
     #fetch comments of blog with id = id
-    return limit
-    return {'data':{'1', '2'}}
+    return {'data': f'{limit} comments from blog {id}'}
 
 #use- python -m uvicorn main:app --reload
 #localhost:8000/docs
