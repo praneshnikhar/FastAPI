69deed09d38bfd081191ae8f6860db852dae1c48
praneshnikhar <praneshnikhar@gmail.com>
2025-10-08 11:16:25 +0530

    oauth2

---
+++ routers/oauth2.py
@@ -0,0 +1,10 @@
+from jose import JWTError, jwt
+
+#secret key 
+#algorithm
+#expiration time
+
+
+SECRET_KEY = "09ssi234b139sndb194b123i8sddn819X09ZU12E8HX8H812"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINIUTES = 30
